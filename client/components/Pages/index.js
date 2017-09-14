a0bf3ec9a82f9120658a42d9fe4759342b38da4c
Seth <itz.saga@gmail.com>
2017-09-14 17:57:16 -0500

    Remove Conduct placeholder from Pages index.

--- client/components/Pages/index.js
+++ client/components/Pages/index.js
@@ -17,23 +17,6 @@ export const Terms = () => {
   );
 };
 
-export const Conduct = () => {
-  return (
-    <div className="container is-fluid">
-      <h1 className="masthead">Code of Conduct</h1>
-      <p>
-        We need to draft a Conduct page.{" "}
-        <a
-          href="https://github.com/egdelwonk/nashdev-jobs/issues/20"
-          target="_blank"
-        >
-          Would you like to help?
-        </a>
-      </p>
-    </div>
-  );
-};
-
 export const Privacy = () => {
   return (
     <div className="container is-fluid">
