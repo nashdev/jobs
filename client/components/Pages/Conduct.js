9407edcae525f23da24ca899bf55dd7b3ea174ab
Seth <itz.saga@gmail.com>
2017-09-14 18:49:11 -0500

    Create & fill Conduct component.

---
+++ client/components/Pages/Conduct.js
@@ -0,0 +1,72 @@
+import React from "react";
+
+const Conduct = () => {
+  return(
+    <div>
+      <section className="hero is-medium is-primary is-bold">
+        <div className="hero-body">
+          <div className="container is-fluid">
+            <h1 className="title">Code of Conduct.</h1>
+          </div>
+        </div>
+      </section>
+      <section className="section">
+        <div className="container is-fluid">
+          <div className="content">
+            <h3>Our Pledge</h3>
+            <p>In the interest of fostering an open and welcoming environment, we as contributors and maintainers pledge to making participation in our project and our community a harassment-free experience for everyone, regardless of age, body size, disability, ethnicity, gender identity and expression, level of experience, nationality, personal appearance, race, religion, or sexual identity and orientation.</p>
+
+            <h3>Our Standards</h3>
+            <p>Examples of behavior that contributes to creating a positive environment include:</p>
+            <ul>
+              <li>Using welcoming and inclusive language</li>
+              <li>Being respectful of differing viewpoints and experiences</li>
+              <li>Gracefully accepting constructive criticism</li>
+              <li>Focusing on what is best for the community</li>
+              <li>Showing empathy towards other community members</li>
+            </ul>
+
+            <p>Examples of unacceptable behavior by participants include:</p>
+            <ul>
+              <li>The use of sexualized language or imagery and unwelcome sexual attention or advances</li>
+              <li>Trolling, insulting/derogatory comments, and personal or political attacks</li>
+              <li>Public or private harassment</li>
+              <li>Publishing others&rsquo; private information, such as a physical or electronic address, without explicit permission</li>
+              <li>Other conduct which could reasonably be considered inappropriate in a professional setting</li>
+            </ul>
+
+            <h3>Our Responsibilities</h3>
+            <p>Project maintainers are responsible for clarifying the standards of acceptable behavior and are expected to take appropriate and fair corrective action in response to any instances of unacceptable behavior.</p>
+            <p>Project maintainers have the right and responsibility to remove, edit, or reject comments, commits, code, wiki edits, issues, and other contributions that are not aligned to this Code of Conduct, or to ban temporarily or permanently any contributor for other behaviors that they deem inappropriate, threatening, offensive, or harmful.</p>
+
+            <h3>Scope</h3>
+            <p>This Code of Conduct applies both within project spaces and in public spaces when an individual is representing the project or its community. Examples of representing a project or community include using an official project e-mail address, posting via an official social media account, or acting as an appointed representative at an online or offline event. Representation of a project may be further defined and clarified by project maintainers.</p>
+
+            <h3>Enforcement</h3>
+            <p>Instances of abusive, harassing, or otherwise unacceptable behavior may be reported by contacting the project team on{" "}
+              <a
+                href="https://nashdev.com"
+                target="_blank"
+              >
+              NashDev Slack
+              </a>{" "}
+              network channel #jobs-community. Or directly to @wgolden on the NashDev Slack network. All complaints will be reviewed and investigated and will result in a response that is deemed necessary and appropriate to the circumstances. The project team is obligated to maintain confidentiality with regard to the reporter of an incident. Further details of specific enforcement policies may be posted separately.</p>
+            <p>Project maintainers who do not follow or enforce the Code of Conduct in good faith may face temporary or permanent repercussions as determined by other members of the project&rsquo;s leadership.</p>
+
+            <h3>Attribution</h3>
+            <p>This Code of Conduct is adapted from the Contributor Covenant, version 1.4, available at{" "}
+              <a
+                href="https://www.contributor-covenant.org/version/1/4/code-of-conduct/"
+                target="_blank"
+              >
+                https://www.contributor-covenant.org/version/1/4/code-of-conduct/
+              </a>
+            </p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Conduct;
