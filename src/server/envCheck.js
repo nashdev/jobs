fe9127ffb32e43a7c6162323ce24c0ca3cfdd696
maxbeizer <max.beizer@gmail.com>
2018-12-23 16:50:06 -0600

    Add comment about the rationale for such a function

--- src/server/envCheck.js
+++ src/server/envCheck.js
@@ -12,6 +12,14 @@ const missingRequiredConfigs = (env) => {
   return [];
 };
 
+// Why? This is to avoid the rake step
+// (https://media.giphy.com/media/ghbWAmMQXZWV2/giphy.gif) of required
+// environment variables not being set. In some cases those values fallback to
+// defaults, but this function helps us fail fast so that it's easier to debug
+// if something is missing that's required. As someone who has had to try to
+// debug these issues only to find out it was a typo in an env var, I now try to
+// add something like this to every project I work on.
+//
 // Why pass in the env? Injecting the object will make this easier to test if we
 // wanted to unit test thist thing, as it would not require munging the test
 // environment.
